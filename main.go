39e6eea519ebe0ece22c78bd63207a8e29513705
yo-tak <yo.tak0812@gmail.com>
2018-10-24 00:29:43 +0900

    enable english mode and refactor logic to wait twitter window

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -12,12 +14,19 @@ import (
 type statusCode int
 
 const (
-	success          statusCode = iota
-	unsufficientCfg  statusCode = iota
-	driverError      statusCode = iota
-	signinFailed     statusCode = iota
-	recruitPageError statusCode = iota
-	otherFailure     statusCode = iota
+	success statusCode = iota
+	unsufficientCfg
+	driverError
+	signinFailed
+	recruitPageError
+	otherFailure
+)
+
+type lang string
+
+const (
+	english  lang = "en"
+	japanese lang = "ja"
 )
 
 func main() {
@@ -91,24 +100,50 @@ func supportOffers(page *agouti.Page) error {
 	return nil
 }
 
+func getLocale(page *agouti.Page) (lang, error) {
+	cookies, err := page.GetCookies()
+	if err != nil {
+		return "", err
+	}
+	for _, cookie := range cookies {
+		if cookie.Name == "locale" {
+			switch cookie.Value {
+			case "en":
+				return english, nil
+			case "ja":
+				return japanese, nil
+			default:
+				return "", errors.New(fmt.Sprintf("no language found for locale: %v", cookie.Value))
+			}
+		}
+	}
+	log.Println("no locale set to cookie. set language to japanese.")
+	return japanese, nil
+}
+
 func supportOffer(elem *api.Element, page *agouti.Page, mainWindow *api.Window) error {
+	language, err := getLocale(page)
+	if err != nil {
+		return err
+	}
 	// open support dialog and click support by twitter
 	elem.Click()
 	supportDialog := page.FindByID("wtd-modal-portal__default")
-	if err := supportDialog.FindByButton("Twitterで応援").Click(); err != nil {
+
+	supportButtonCaption := ""
+	if language == japanese {
+		supportButtonCaption = "Twitterで応援"
+	} else {
+		supportButtonCaption = "Recommend on Twitter"
+	}
+	supportButton := supportDialog.FindByButton(supportButtonCaption)
+	if err := supportButton.Click(); err != nil {
 		log.Printf("Failed to click support button for elem: %v, err: %v", elem, err)
 		return err
 	}
+	proceedTwitterWindow(page)
 
-	time.Sleep(1500 * time.Millisecond) // wait until twitter window is ready
-	// close twitter window
 	// TODO should improve logic to handle windows
-	if err := page.NextWindow(); err != nil { // move to new Twitter window
-		return err
-	}
-	if err := page.CloseWindow(); err != nil { // close Twitter window
-		return err
-	}
 	if err := page.Session().SetWindow(mainWindow); err != nil { // move back to recruitment page window
 		return err
 	}
@@ -116,12 +151,44 @@ func supportOffer(elem *api.Element, page *agouti.Page, mainWindow *api.Window) 
 
 	// close support dialog. when it's your first time to supported the offer,
 	// the caption will be "応援しない". otherwise it will be "閉じる""
+	closeButtonCaption := ""
+	if language == japanese {
+		closeButtonCaption = "閉じる"
+	} else {
+		closeButtonCaption = "Close"
+	}
 	closeButton := supportDialog.FindByButton("応援しない")
 	if count, err := closeButton.Count(); err != nil || count == 0 {
-		closeButton = supportDialog.FindByButton("閉じる")
+		closeButton = supportDialog.FindByButton(closeButtonCaption)
 	}
 	if err := closeButton.Click(); err != nil {
 		log.Printf("Failed to click close button for elem: %v, err: %v", elem, err)
+		return err
+	}
+	return nil
+}
+
+func proceedTwitterWindow(page *agouti.Page) error {
+	if err := page.NextWindow(); err != nil { // move to new Twitter window
+		return err
+	}
+	twitterReady := make(chan bool)
+	go func() {
+		for trial := 0; trial < 20; trial++ {
+			time.Sleep(1000 * time.Millisecond)
+			tweetForm := page.Find("input[type='submit']")
+			if tweetForm != nil {
+				twitterReady <- true
+				return
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+		twitterReady <- false
+	}()
+	if !<-twitterReady {
+		return errors.New("failed to open twitter form")
+	}
+	if err := page.CloseWindow(); err != nil { // close Twitter window
 		return err
 	}
 	return nil
